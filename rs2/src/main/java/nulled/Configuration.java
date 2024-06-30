1a6e47456567daecfb62226f1013cf6a30646ec4
Harley <hg@harleygilpin.com>
2024-06-29 23:43:23 -0700

    feat: updated server connection details to connect to 04scaper.ca

--- rs2/src/main/java/nulled/Configuration.java
+++ rs2/src/main/java/nulled/Configuration.java
@@ -7,7 +7,7 @@ package nulled;
 public class Configuration {
     public static boolean localhost = false;
 
-    public static final String URL = "https://w2.225.2004scape.org";
-    public static final int PORT_OFFSET = 3;
+    public static final String URL = "https://w1.04scaper.ca";
+    public static final int PORT_OFFSET = 0;
     public static final int PORT = 43594;
 }
